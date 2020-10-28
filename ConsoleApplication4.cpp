97ec90a9afe494b8c0265bfabbcf1d2280222bba
LizaZaharenko363 <liza.zaharenko25@gmail.com>
2020-10-28 21:06:44 +0200

    new code

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <iomanip>
+#include "windows.h"
+#define _USE_MATH_DEFINES   
+#include "math.h" 
+
+using namespace std;
+
+int main()
+{
+	double y, x1, x2, x, d;
+	cin >> x1 >> x2 >> d;
+	x = x1;
+	while (x <= x2) {
+		y = exp(x) + abs(x) + pow(x,2);
+		cout << setw(10) << x << setw(10) << y << endl;
+		x += d;
+	} ;
+}
